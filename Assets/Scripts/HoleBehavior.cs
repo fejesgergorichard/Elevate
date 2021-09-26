385391c778f07347232145f4b892c7ee253c41a9
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-09-26 22:16:18 +0200

    Initial commit

---
+++ Assets/Scripts/HoleBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HoleBehavior : MonoBehaviour
+{
+    private CancellationTokenSource cts;
+    public Vector3 TargetScale;
+    public float Duration;
+    public bool Goal = false;
+
+    void Start()
+    {
+        cts = new CancellationTokenSource();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!Goal)
+            {
+                StartCoroutine(Rescale(TargetScale, Duration, other, cts.Token));
+                StartCoroutine(MoveInside(transform.position, Duration, other, cts.Token));
+            }
+            else
+            {
+                StartCoroutine(Rescale(new Vector3(5, 5, 5), Duration, other, cts.Token));
+                StartCoroutine(MoveInside(transform.position, Duration, other, cts.Token));
+                other.attachedRigidbody.isKinematic = false;
+                Debug.Log("YEET");
+            }
+
+        }
+    }
+
+    private IEnumerator Rescale(Vector3 targetValue, float duration, Collider2D other, CancellationToken token)
+    {
+        Vector3 startValue = other.transform.localScale;
+        var x = startValue.x;
+        var y = startValue.y;
+        var z = startValue.z;
+
+
+        float time = 0;
+
+        while (time < duration)
+        {
+            token.ThrowIfCancellationRequested();
+
+            var newX = Mathf.Lerp(x, targetValue.x, time / duration);
+            var newY = Mathf.Lerp(y, targetValue.y, time / duration);
+            var newZ = Mathf.Lerp(z, targetValue.z, time / duration);
+
+            other.transform.localScale = new Vector3(newX, newY, newZ);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("SampleScene");
+
+        other.transform.localScale = targetValue;
+    }
+
+    private IEnumerator MoveInside(Vector3 targetValue, float duration, Collider2D other, CancellationToken token)
+    {
+        Vector3 startValue = other.transform.position;
+        var x = startValue.x;
+        var y = startValue.y;
+        var z = startValue.z;
+
+
+        float time = 0;
+
+        while (time < duration)
+        {
+            token.ThrowIfCancellationRequested();
+
+            var newX = Mathf.Lerp(x, targetValue.x, time / duration);
+            var newY = Mathf.Lerp(y, targetValue.y, time / duration);
+            var newZ = Mathf.Lerp(z, targetValue.z, time / duration);
+
+            other.transform.position = new Vector3(newX, newY, newZ);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("SampleScene");
+
+        other.transform.position = targetValue;
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public CharacterController2D Left;
+    public CharacterController2D Right;
+	[Space]
+	public float Speed = 2f;
+
+	private bool tapLeft;
+	private bool tapRight;
+	private DeviceType deviceType;
+
+
+	void Start()
+    {
+		Application.targetFrameRate = 80;
+		deviceType = SystemInfo.deviceType;
+    }
+
+    void Update()
+    {
+		if (deviceType == DeviceType.Handheld)
+		{
+			tapLeft = tapRight = false;
+
+			if (Input.touches.Length != 0)
+			{
+				// "touches[0]" - first finger to touch
+				CheckTouchSide(Input.touches[0]);
+
+				if (Input.touches.Length > 1)
+				{
+					// "touches[1]" - second finger to touch
+					CheckTouchSide(Input.touches[1]);
+				}
+			}
+		}
+		else if (deviceType == DeviceType.Desktop)
+		{
+			tapLeft = Input.GetKey(KeyCode.A);
+			tapRight = Input.GetKey(KeyCode.D);
+		}
+
+
+		// Process the inputs
+		if (tapLeft)
+		{
+			Left.Move(Speed * Time.deltaTime, false, false);
+		}
+		if (tapRight)
+		{
+			Right.Move(Speed * Time.deltaTime, false, false);
+		}
+	}
+
+	private void CheckTouchSide(Touch touch)
+	{
+		if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+		{
+			if (touch.position.x < Screen.width / 2)
+			{
+				tapLeft = true;
+			}
+			else if (touch.position.x > Screen.width / 2)
+			{
+				tapRight = true;
+			}
+		}
+	}
+}
